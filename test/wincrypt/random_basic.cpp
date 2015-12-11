11b20747bc7daa43db75f28f660830a2833f9bfb
Flysha <sdflysha@qq.com>
2015-12-11 17:53:07 +0800

    +random primitive test

--- test/wincrypt/random_basic.cpp
+++ test/wincrypt/random_basic.cpp
@@ -1,6 +1,7 @@
 #include <CppUnitTest.h>
 #include <wincrypt\wincrypt.h>
 #include <array>
+#include <sstream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace wincrypt;
@@ -8,16 +9,29 @@ using namespace wincrypt;
 TEST_CLASS(Wincrypt_Random)
 {
 public:
-	TEST_METHOD(Basic)
+	TEST_METHOD(ArrayBuffer)
 	{
 		auto p = open_provider(BCRYPT_RNG_ALGORITHM);
 
 		std::array<byte, 16> buffer;
+		random(p, buffer);
+	}
 
-		random(p, buffer);
-		random(p, buffer);
-		random(p, buffer);
-		random(p, buffer);
-		random(p, buffer);
+	TEST_METHOD(Primitive)
+	{
+		auto p = open_provider(BCRYPT_RNG_ALGORITHM);
+		std::stringstream ss;
+
+		for (auto i = 1; i <= 100; ++i)
+		{
+			byte a;
+			random(p, a);
+			ss << (int)a << "\t";
+
+			if (i % 10 == 0) ss << std::endl;
+		}
+
+		Logger::WriteMessage(ss.str().c_str());
+		
 	}
 };
